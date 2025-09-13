0d02c49cce6e2bd35e0fe0a9a108a9618bad7246
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2025-09-13 12:17:18 +0200

    Day 8. Add project placeholder

---
+++ Day8/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    private const string INPUT = @"..\..\..\test.txt";
+    //private const string INPUT = @"..\..\..\input.txt";
+
+    static void Main()
+    {
+        Console.WriteLine("\nPress any key to exit . . .");
+        Console.Read();
+    }
+}
