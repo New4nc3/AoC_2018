f4b205ccc1e46f6dd027bc17f6894bea11e4bea7
Oleksandr Dzhurabaiev <dow1996@ukr.net>
2025-11-29 12:27:09 +0100

    Day 9. Add initial solution template

---
+++ Day9/MarbleProcessor.cs
@@ -0,0 +1,13 @@
+class MarbleProcessor
+{
+    public int Players { get; }
+    public int LastMarbleValue { get; }
+
+    public MarbleProcessor(int players, int lastMarbleValue) =>
+        (Players, LastMarbleValue) = (players, lastMarbleValue);
+
+    public void Part1()
+    {
+        
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    private const string INPUT = @"..\..\..\test.txt";
+    //private const string INPUT = @"..\..\..\input.txt";
+
+    static void Main()
+    {
+        var input = ReadFromFile(INPUT);
+        MarbleProcessor marbleProcessor = new MarbleProcessor(input.Players, input.LastMarble);
+        marbleProcessor.Part1();
+
+        Console.WriteLine($"{input.Players}, {input.LastMarble}");
+    }
+
+    static (int Players, int LastMarble) ReadFromFile(string filename)
+    {
+        string[] inputWords;
+        using (StreamReader streamReader = new StreamReader(filename))
+            inputWords = streamReader.ReadLine()
+                ?.Split(" ") ?? new string[0];
+
+        return (Convert.ToInt32(inputWords[0]), Convert.ToInt32(inputWords[6]));
+    }
+}
